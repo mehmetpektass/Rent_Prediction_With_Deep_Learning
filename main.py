0a432c1f7fab284d0a29450079719eb1a6b755c6
mehmetpektass <pektasmehmett1184@gmail.com>
2024-12-10 00:47:05 +0300

    Extract location parts (city, district, neighborhood) from 'list-view-location' column

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import numpy as np
 import pandas as pd
 
+
+# Load CSV data into a DataFrame
 df = pd.read_csv("scraped_data.csv")
-df.info()
-df.head()
+df.info()  
+df.head()  
 
 
+# Function to drop specified columns from the DataFrame
 def drop_columns(df, columns):
     for column in columns:
         try:
-            df.drop([column] , axis=1 , inplace=True)
-            help(df.drop)
+            df.drop([column], axis=1, inplace=True) 
         except:
             print("An error happened while trying to drop the columns")
 
 
-cols = ['img-link href', 'photo-count','list-view-date', 'list-view-title', 'left' , 'he-lazy-image src',  'img-wrp href', 'he-lazy-image src 2', 'listing-card--owner-info__firm-name', 'listing-card--owner-info__name', 'img-wrp href 2', 'he-lazy-image src 3', 'wp-btn']
-drop_columns(df, cols)
+# List of columns to drop
+cols = ['img-link href', 'photo-count', 'list-view-date', ...]
+drop_columns(df, cols) 
+df.info() 
 
-print(df.info())
-df.head(5)
+
+# Extract location parts (city, district, neighborhood) from 'list-view-location' column
+df["city"] = df["list-view-location"].str.split("/").str[0]
+df["district"] = df["list-view-location"].str.split("/").str[1]
+df["neighborhood"] = df["list-view-location"].str.split("/").str[2]
+df["neighborhood"] = df["neighborhood"].apply(lambda x: " ".join(x))
+
+df.info()  
+df.head() 
+
+
+# Drop the 'list-view-location' column after splitting its values
+drop_columns(df, ["list-view-location"])
