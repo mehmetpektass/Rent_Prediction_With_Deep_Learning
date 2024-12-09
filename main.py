5986fa2bcd273a0e8d30ed67220021859e15311b
mehmetpektass <pektasmehmett1184@gmail.com>
2024-12-09 22:42:36 +0300

    created drop columns function

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ df.info()
 df.head()
 
 
+def drop_columns(df, columns):
+    for column in columns:
+        try:
+            df.drop([column] , axis=1 , inplace=True)
+            help(df.drop)
+        except Exception as e:
+            print("An error happened while trying to drop the columns")
+
