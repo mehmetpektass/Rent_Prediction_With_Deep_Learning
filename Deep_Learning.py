fc1f37010c6c23d23f50cbfc85fce2fda0c01fd5
mehmetpektass <pektasmehmett1184@gmail.com>
2024-12-13 12:30:27 +0300

    converted columns with loop of a function

--- Deep_Learning.py
+++ Deep_Learning.py
@@ -6,4 +6,20 @@ from sklearn.compose import ColumnTransformer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 
+df = pd.read_csv("data_cleaned.csv")
+
+
+# Convert columns with loop of a function
+categorical_features = ["district" , "neighborhood"]
+numerical_features = ["room", "living_room", "area", "age", "floor", "price"]
+
+def convert_columns(df, columns,type):
+    for column in columns:
+        df[column] = df[column].astype(type)
+        
+convert_columns(df, categorical_features, "category")
+convert_columns(df, numerical_features, "int")
+
+df.info()
+    
 
