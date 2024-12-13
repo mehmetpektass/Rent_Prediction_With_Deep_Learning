06dac2337fa15e772b8fdefddbd859c5708d49a7
mehmetpektass <pektasmehmett1184@gmail.com>
2024-12-13 12:04:57 +0300

    imported libraries on deep learning file

---
+++ Deep_Learning.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+
