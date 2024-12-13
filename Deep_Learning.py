bd0441280888af6b894b28dfa7b36ee1bb527da5
mehmetpektass <pektasmehmett1184@gmail.com>
2024-12-13 16:29:48 +0300

    defined the natural network model

--- Deep_Learning.py
+++ Deep_Learning.py
@@ -8,7 +8,6 @@ from tensorflow.keras.layers import Dense, Dropout
 
 df = pd.read_csv("data_cleaned.csv")
 
-
 # Convert columns with loop of a function
 categorical_features = ["district" , "neighborhood"]
 numerical_features = ["room", "living_room", "area", "age", "floor"]
@@ -21,16 +20,26 @@ convert_columns(df, categorical_features, "category")
 convert_columns(df, numerical_features, int)
 
 
+#Split the dataset as X nad y
 X = df.drop("price", axis=1)
 y = df["price"]
 
-# Create pipeline and fit-transform
+
+# Create pipeline and fit-transform and finally train-test sets
 full_pipeline = ColumnTransformer([
     ("num", StandardScaler(), numerical_features),
     ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_features)
 ])
 
 X_prepared = full_pipeline.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_prepared, y, test_size=0.2, random_state=42)
 
     
-
+# Define the neural network model
+model = Sequential([
+    Dense(128, activation="relu", input_shape=(X_train.shape[1],)),
+    Dropout(0.3),
+    Dense(64, activation="relu"),
+    Dropout(0.3),
+    Dense(1, activation="linear"),
+])
